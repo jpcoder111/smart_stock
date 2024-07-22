acbaffb15887686b201996c68d34f492b7f3af59
jpcoder111 <juan.pablo.contreras.g@gmail.com>
2024-07-22 10:35:22 -0400

    fix

--- apps/webhooks/views.py
+++ apps/webhooks/views.py
@@ -1,10 +1,7 @@
-import json
-import logging
+# apps/webhooks/views.py
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-
-# logger = logging.getLogger('webhook')
 
 @csrf_exempt
 @require_http_methods(["GET", "POST", "PUT", "DELETE", "PATCH"])
